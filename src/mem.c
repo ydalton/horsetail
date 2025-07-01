336f8aaff09d3482622201e32234c566f48bdde4
Yussef Dalton <yussef.dalton@gmail.com>
2025-07-01 16:48:37 +0200

    Move files around

--- src/mem.c
+++ src/mem.c
@@ -1,4 +1,29 @@
+#include "horsetail/horsetail.h"
 #include "horsetail/mem.h"
+#include "horsetail/impl.h"
+
+void MemInit(void)
+{
+    HtLog("Mem: initialized\n");
+}
+
+/* TODO: own implementations */
+
+void *MemAlloc(usize size, MemAllocTag tag)
+{
+    void *memory = NULL;
+    HT_UNUSED(tag);
+
+    memory = ImplMemAlloc(size);
+    HtZeroMemory(memory, size);
+
+    return memory;
+}
+
+void MemFree(void *memory)
+{
+    ImplMemFree(memory);
+}
 
 void MemArena_Init(MemArena *arena, usize size)
 {
